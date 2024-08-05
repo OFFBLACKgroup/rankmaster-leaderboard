7199865b52b2ae1acb43fc4b58ca15f3289e7cf9
OFFBLACKgroup <offblack.group@gmail.com>
2024-08-05 18:14:02 +0200

    leaderboard server initiating

--- index.js
+++ index.js
@@ -1,16 +1,27 @@
 import express from 'express'
 import 'dotenv/config'
+import Ably from 'ably'
+import { createClient } from 'jsr:@supabase/supabase-js@2'
 
 
 const express = require('express')
 const app = express()
+const supabase = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_KEY)
 
-  // const subscription = supabase
-  //   .channel('public:leaderboard')
-  //   .on('postgres_changes', { event: '*', schema: 'public', table: 'leaderboard' }, (payload) => {
-  //     session.push(JSON.stringify(payload))
-  //   })
-  //   .subscribe();
+const ably = new Ably.Realtime(process.env.ABLY_ADMIN_KEY)
+const leaderboard = ably.channels.get('leaderboard')
+
+supabase
+  .channel('public:leaderboard')
+  .on('postgres_changes', { event: '*', schema: 'public', table: 'leaderboard' }, async (payload) => {
+    try {
+      await leaderboard.publish('admin', JSON.stringify(payload));
+      console.log('Published update to Ably:', payload);
+    } catch (error) {
+      console.error('Error publishing to Ably:', error);
+    }
+  })
+  .subscribe()
 
 app.listen(3000, () => {
   console.log(`Example app listening on port ${port}`)
