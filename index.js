c1a5f3c838e3f4cdfd850c011b986e9fcb8bc401
OFFBLACKgroup <offblack.group@gmail.com>
2024-08-05 18:53:29 +0200

    root page test

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const supabase = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_KEY
 const ably = new Ably.Realtime(process.env.ABLY_ADMIN_KEY)
 const leaderboard = ably.channels.get('leaderboard')
 
-app.get("/", (req, res) => res.send("RankMaster Leaderboard Updater"));
+app.get("", (req, res) => res.send("RankMaster Leaderboard Updater"));
 
 supabase
   .channel('public:leaderboard')
