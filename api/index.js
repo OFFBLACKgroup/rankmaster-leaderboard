e8bbf3a335efd6d1593dc4a9e7e81fb7d631e14a
OFFBLACKgroup <offblack.group@gmail.com>
2024-08-05 19:09:22 +0200

    root page test v4

--- api/index.js
+++ api/index.js
@@ -4,24 +4,24 @@ import Ably from 'ably'
 import { createClient } from 'jsr:@supabase/supabase-js@2'
 
 const app = express()
-const supabase = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_KEY)
+// const supabase = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_KEY)
 
-const ably = new Ably.Realtime(process.env.ABLY_ADMIN_KEY)
-const leaderboard = ably.channels.get('leaderboard')
+// const ably = new Ably.Realtime(process.env.ABLY_ADMIN_KEY)
+// const leaderboard = ably.channels.get('leaderboard')
 
-app.get("", (req, res) => res.send("RankMaster Leaderboard Updater"));
+app.get("/", (req, res) => res.send("RankMaster Leaderboard Updater"));
 
-supabase
-  .channel('public:leaderboard')
-  .on('postgres_changes', { event: '*', schema: 'public', table: 'leaderboard' }, async (payload) => {
-    try {
-      await leaderboard.publish('admin', JSON.stringify(payload));
-      console.log('Published update to Ably:', payload);
-    } catch (error) {
-      console.error('Error publishing to Ably:', error);
-    }
-  })
-  .subscribe()
+// supabase
+//   .channel('public:leaderboard')
+//   .on('postgres_changes', { event: '*', schema: 'public', table: 'leaderboard' }, async (payload) => {
+//     try {
+//       await leaderboard.publish('admin', JSON.stringify(payload));
+//       console.log('Published update to Ably:', payload);
+//     } catch (error) {
+//       console.error('Error publishing to Ably:', error);
+//     }
+//   })
+//   .subscribe()
 
 app.listen(3000, () => {
   console.log(`Example app listening on port ${port}`)
